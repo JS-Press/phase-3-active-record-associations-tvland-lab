440f339b09f6e18761acbfd3b627052220224b40
Jarad Solomon <jaradsolomon@gmail.com>
2021-10-21 15:35:55 -0500

    CA

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,11 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+
+    create_table :actors do |c|
+      c.string :first_name 
+      c.string :last_name
+    end
+
   end
 end
